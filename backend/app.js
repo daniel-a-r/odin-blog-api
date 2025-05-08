bd101a5905f774f30b64ebab7f010a20042bf667
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-05-08 04:59:43 -0400

    test token creation

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ import passport from 'passport';
 import passportJwt from 'passport-jwt';
 import prisma from './prisma/client.js';
 import helemt from 'helmet';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
 const JwtStrategy = passportJwt.Strategy;
 const ExtractJwt = passportJwt.ExtractJwt;
@@ -13,27 +15,26 @@ app.disable('x-powered-by');
 app.use(helemt());
 app.use(express.urlencoded({ extended: false }));
 
-const opts = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken,
-  secretOrKey: 'secret',
-};
+// const opts = {
+//   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken,
+//   secretOrKey: process.env.JWT_SECRET,
+// };
 
-passport.use(
-  new JwtStrategy(opts, async (jwt_payload, done) => {
-    console.log(jwt_payload);
-    done(null, false);
-  }),
-);
+// passport.use(
+//   new JwtStrategy(opts, async (jwt_payload, done) => {
+//     console.log(jwt_payload);
+//     done(null, false);
+//   }),
+// );
 
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
   console.log(req.body);
+  const payload = { username };
+  const opts = { expiresIn: '30m' };
+  const token = jwt.sign(payload, process.env.JWT_SECRET, opts);
   res.json({
-    message: 'login route',
-    body: {
-      username,
-      password,
-    },
+    token,
   });
 });
 
