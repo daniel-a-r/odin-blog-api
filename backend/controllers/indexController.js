6e385a2868f58bde4bafffc7dece9d454f73a7d6
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-05-26 05:07:27 -0400

    validate sign up and create user

--- backend/controllers/indexController.js
+++ backend/controllers/indexController.js
@@ -1,12 +1,67 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import prisma from '../prisma/client.js';
+import { ExpressValidator, validationResult } from 'express-validator';
 
-const signUpPost = (req, res) => {
+const { body } = new ExpressValidator({
+  isUsernameNotInUse: async (value) => {
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        username: value,
+      },
+    });
+    if (existingUser) {
+      throw new Error('Username already taken');
+    }
+  },
+  passwordsMatch: (value, { req }) => {
+    if (req.body.password !== value) {
+      throw new Error('Passwords do not match');
+    }
+    return true;
+  },
+});
+
+const validateSignUp = [
+  body('username')
+    .trim()
+    .toLowerCase()
+    .isAlphanumeric()
+    .withMessage('Username must only contain letters and numbers.')
+    .isUsernameNotInUse(),
+  body('confirmPassword').passwordsMatch(),
+];
+
+const checkSignUpValidationErrors = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
+const createUser = async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await prisma.user.create({
+    data: {
+      username: username,
+      password: hashedPassword,
+    },
+  });
+  const payload = {
+    id: user.id,
+    username: user.username,
+    role: user.role,
+  };
+  const opts = { expiresIn: '30m' };
+  const token = jwt.sign(payload, process.env.JWT_SECRET, opts);
   res.json({
-    message: 'signed up!',
+    token,
   });
 };
+
+const signUpPost = [...validateSignUp, checkSignUpValidationErrors, createUser];
 
 const loginPost = (req, res) => {
   const { username, password } = req.body;
