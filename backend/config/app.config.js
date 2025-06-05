6cbe0246c43f5d0fec3e58a58d40851caf521ec1
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-06-05 00:42:21 -0400

    install and use cookie-parser

--- backend/config/app.config.js
+++ backend/config/app.config.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import helemt from 'helmet';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.disable('x-powered-by');
 
 app.use(helemt());
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 export default app;
