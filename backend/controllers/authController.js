9fc2f8368b1a5939414d4723efeba35763aa1aa8
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-06-05 00:45:08 -0400

    store refresh token in cookie and abstract token creation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,25 @@ import bcrypt from 'bcryptjs';
 import prisma from '../prisma/client.js';
 import { ExpressValidator, validationResult } from 'express-validator';
 
-const ACCESS_TOKEN_EXPIRES_IN = '30m';
+const ACCESS_TOKEN_EXPIRES_IN = '30 minutes';
+const REFRESH_TOKEN_EXPIRES_IN = '7 days';
+const COOKIE_OPTS = {
+  maxAge: 1000 * 60 * 60 * 24 * 7, // 1 second * 60 seconds * 60 minutes * 24 hours * 7 days
+  secure: true,
+  signed: true,
+};
+
+const createAccessToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: ACCESS_TOKEN_EXPIRES_IN,
+  });
+};
+
+const createRefreshToken = (payload) => {
+  return jwt.sign(payload, process.env.REFRESH_SECRET, {
+    expiresIn: REFRESH_TOKEN_EXPIRES_IN,
+  });
+};
 
 const { body } = new ExpressValidator({
   isUsernameNotInUse: async (value) => {
@@ -45,34 +63,57 @@ const checkSignUpValidationErrors = (req, res, next) => {
 const createUser = async (req, res) => {
   const { username, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
+
   const user = await prisma.user.create({
     data: {
       username: username,
       password: hashedPassword,
     },
   });
+
   const payload = {
     id: user.id,
     username: user.username,
     role: user.role,
   };
-  const opts = { expiresIn: ACCESS_TOKEN_EXPIRES_IN };
-  const token = jwt.sign(payload, process.env.JWT_SECRET, opts);
-  res.json({
-    token,
-  });
+
+  const accessToken = createAccessToken(payload);
+  const refreshToken = createRefreshToken(payload);
+
+  res.cookie('refreshToken', refreshToken, COOKIE_OPTS).json({ accessToken });
 };
 
 const signUpPost = [...validateSignUp, checkSignUpValidationErrors, createUser];
 
-const loginPost = (req, res) => {
+const loginPost = async (req, res) => {
   const { username, password } = req.body;
-  const payload = { username };
-  const opts = { expiresIn: ACCESS_TOKEN_EXPIRES_IN };
-  const token = jwt.sign(payload, process.env.JWT_SECRET, opts);
-  res.json({
-    token,
-  });
+
+  try {
+    const user = await prisma.user.findUniqueOrThrow({
+      where: {
+        username,
+      },
+    });
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      throw new Error('invalid password');
+    }
+
+    const payload = {
+      id: user.id,
+      username: user.username,
+      role: user.role,
+    };
+
+    const accessToken = createAccessToken(payload);
+    const refreshToken = createRefreshToken(payload);
+
+    res.cookie('refreshToken', refreshToken, COOKIE_OPTS).json({ accessToken });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: 'invalid username or password' });
+  }
 };
 
 const refreshGet = (req, res) => {
