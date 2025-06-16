4660c516389028a1c5c5944c46430acd8cc83a1e
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-06-16 18:32:10 -0400

    install prettier and format

--- frontend/author/src/App.jsx
+++ frontend/author/src/App.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import './App.css'
+import './App.css';
 
 function App() {
   const signUp = async (formData) => {
@@ -12,24 +12,29 @@ function App() {
       return alert('passwords do not match');
     }
 
-    const {data} = await axios.post('http://localhost:3000/sign-up', body);
+    const { data } = await axios.post('http://localhost:3000/sign-up', body);
     console.log(data);
-  }
+  };
 
   return (
     <>
       <h1>Sign Up</h1>
       <form action={signUp}>
-        <label htmlFor="username">Username:</label>
-        <input type="text" name="username" id="username" required />
-        <label htmlFor="password">Password:</label>
-        <input type="password" name="password" id="password" required/>
-        <label htmlFor="confirm-password">Confirm Passwowrd:</label>
-        <input type="password" name="confirmPassword" id="confirm-password" required />
-        <button type="submit">Sign Up</button>
+        <label htmlFor='username'>Username:</label>
+        <input type='text' name='username' id='username' required />
+        <label htmlFor='password'>Password:</label>
+        <input type='password' name='password' id='password' required />
+        <label htmlFor='confirm-password'>Confirm Passwowrd:</label>
+        <input
+          type='password'
+          name='confirmPassword'
+          id='confirm-password'
+          required
+        />
+        <button type='submit'>Sign Up</button>
       </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
