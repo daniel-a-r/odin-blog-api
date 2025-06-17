2cc09424e847d706910672d81a8aa1203a68269c
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-06-17 06:45:10 -0400

    install eslint prettier plugin

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import json from '@eslint/json';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
@@ -22,4 +23,5 @@ export default defineConfig([
     language: 'json/json',
     extends: ['json/recommended'],
   },
+  eslintPluginPrettierRecommended,
 ]);
