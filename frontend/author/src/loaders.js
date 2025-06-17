329d2b018c9dc9d18a63f6044a405592525d9aed
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-06-17 07:03:07 -0400

    format with prettier

--- frontend/author/src/loaders.js
+++ frontend/author/src/loaders.js
@@ -1,8 +1,10 @@
-import axios from "axios"
-import { POST_ENDPOINT } from "./utils"
+import axios from 'axios';
+import { POST_ENDPOINT } from './utils';
 
 export const dashboardLoader = async () => {
   const accessToken = localStorage.getItem('accessToken');
-  const data = await axios.get(POST_ENDPOINT, { headers: {'Authorization': `Bearer ${accessToken}`}});
+  const data = await axios.get(POST_ENDPOINT, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
   return data;
-}
+};
--- frontend/author/src/routes/dashboard/Dashboard.jsx
+++ frontend/author/src/routes/dashboard/Dashboard.jsx
@@ -1,10 +1,10 @@
-import { useLoaderData } from "react-router";
+import { useLoaderData } from 'react-router';
 
 const Dashboard = () => {
   const { data } = useLoaderData();
   console.log(data);
 
-  return <h1>dashboard</h1>
-}
+  return <h1>dashboard</h1>;
+};
 
 export default Dashboard;
