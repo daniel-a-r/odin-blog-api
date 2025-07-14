880d7d8e350b0a4ce197e3bb964fc14e09358734
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-07-14 05:15:57 -0400

    add const endpoints

--- frontend/author/src/utils/utils.js
+++ frontend/author/src/utils/utils.js
@@ -2,5 +2,7 @@ export const baseURL = import.meta.env.DEV
   ? 'http://localhost:3000/api/v1'
   : 'tbd';
 
-export const LOGIN_ENDPOINT = `${baseURL}/auth/login`;
-export const POST_ENDPOINT = `${baseURL}/author/post`;
+export const LOGIN_ENDPOINT = '/auth/login';
+export const POST_ENDPOINT = '/author/post';
+export const VALIDATE_ENDPOINT = '/auth/validate';
+export const LOGOUT_ENDPOINT = '/auth/logout';
