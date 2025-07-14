51f243de8db64b09ec5e6e7c8c05eaaf24631f5c
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-07-14 05:22:20 -0400

    display post info and link to post editor page

--- frontend/author/src/routes/dashboard/Dashboard.jsx
+++ frontend/author/src/routes/dashboard/Dashboard.jsx
@@ -1,10 +1,43 @@
 import { useLoaderData } from 'react-router';
+import axios from '@/utils/axios';
+import { LOGOUT_ENDPOINT } from '@/utils/utils';
+import { useNavigate, Link } from 'react-router';
 
 const Dashboard = () => {
   const { data } = useLoaderData();
+  const navigate = useNavigate();
   console.log(data);
 
-  return <h1>dashboard</h1>;
+  const handleLogout = async () => {
+    localStorage.removeItem('accessToken');
+    await axios.get(LOGOUT_ENDPOINT, {
+      withCredentials: true,
+    });
+    return navigate('/');
+  };
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <button onClick={handleLogout}>Logout</button>
+      <ul>
+        {data.map((post) => (
+          <Link to={`/dashboard/${post.id}`} key={post.id} state={post}>
+            <li>
+              <p>{post.title}</p>
+              <p>{post.body}</p>
+              <p>Published: {String(post.published)}</p>
+              {post.createdAt !== post.updatedAt ? (
+                <p>Updated at: {post.updatedAt}</p>
+              ) : (
+                <p>Created at: {post.createdAt}</p>
+              )}
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Dashboard;
