450ece93104f4145660c17932aaff573b86394d8
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-07-15 04:09:19 -0400

    add styling

--- frontend/author/src/routes/dashboard/Dashboard.jsx
+++ frontend/author/src/routes/dashboard/Dashboard.jsx
@@ -1,12 +1,11 @@
-import { useLoaderData } from 'react-router';
+import styles from './Dashboard.module.css';
+import { useLoaderData, useNavigate, Link } from 'react-router';
 import axios from '@/utils/axios';
-import { LOGOUT_ENDPOINT } from '@/utils/utils';
-import { useNavigate, Link } from 'react-router';
+import { LOGOUT_ENDPOINT, formatDate } from '@/utils/utils';
 
 const Dashboard = () => {
   const { data } = useLoaderData();
   const navigate = useNavigate();
-  console.log(data);
 
   const handleLogout = async () => {
     localStorage.removeItem('accessToken');
@@ -17,26 +16,29 @@ const Dashboard = () => {
   };
 
   return (
-    <>
-      <h1>Dashboard</h1>
-      <button onClick={handleLogout}>Logout</button>
-      <ul>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1>Posts</h1>
+        <button onClick={handleLogout}>Logout</button>
+      </header>
+      <ul className={styles.ul}>
         {data.map((post) => (
-          <Link to={`/dashboard/${post.id}`} key={post.id} state={post}>
-            <li>
-              <p>{post.title}</p>
+          <Link
+            to={`/dashboard/${post.id}`}
+            key={post.id}
+            state={post}
+            className={styles.link}
+          >
+            <li className={styles.li}>
+              <h2 className={styles.h2}>{post.title}</h2>
               <p>{post.body}</p>
               <p>Published: {String(post.published)}</p>
-              {post.createdAt !== post.updatedAt ? (
-                <p>Updated at: {post.updatedAt}</p>
-              ) : (
-                <p>Created at: {post.createdAt}</p>
-              )}
+              <p>Created at: {formatDate(post.createdAt)}</p>
             </li>
           </Link>
         ))}
       </ul>
-    </>
+    </div>
   );
 };
 
