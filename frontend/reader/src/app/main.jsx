c38677aaa399fed20b4fb41612d5ce5dc2bc7ee0
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-10-14 04:06:07 -0400

    add dark theme from shadcn

--- frontend/reader/src/app/main.jsx
+++ frontend/reader/src/app/main.jsx
@@ -4,11 +4,14 @@ import './index.css';
 import { createBrowserRouter } from 'react-router';
 import { RouterProvider } from 'react-router/dom';
 import routes from '@/app/routes';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 );
