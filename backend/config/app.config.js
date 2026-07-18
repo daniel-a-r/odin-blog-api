e8bad77b0ec179653a24e574d93d66d7e18e6044
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2026-07-17 22:48:22 -0400

    log requests with morgan

--- backend/config/app.config.js
+++ backend/config/app.config.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import helemt from 'helmet';
 import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(helemt());
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(morgan('dev'));
 
 export default app;
