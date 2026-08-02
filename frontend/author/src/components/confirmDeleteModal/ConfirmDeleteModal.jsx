08d8952bd6d4e2de42e10829aaddabaa8fd9f953
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2026-08-02 04:14:10 -0400

    style: add styling to modal

--- frontend/author/src/components/confirmDeleteModal/ConfirmDeleteModal.jsx
+++ frontend/author/src/components/confirmDeleteModal/ConfirmDeleteModal.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import styles from './ConfirmDeleteModal.module.css';
 
 const ConfirmDeleteModal = ({
   isModalOpen,
@@ -10,12 +11,12 @@ const ConfirmDeleteModal = ({
   if (!isModalOpen) return null;
 
   return (
-    <div className='modalOverlay'>
-      <div className='modalContent'>
-        <h2>Are you sure you want to delete?</h2>
-        <h3>{title}</h3>
-        <p>id: {id}</p>
-        <div className='modalButtons'>
+    <div className={styles.modalOverlay}>
+      <div className={styles.modalContent}>
+        <h2 className={styles.h2}>Are you sure you want to delete?</h2>
+        <h3 className={styles.h3}>{title}</h3>
+        <p className={styles.p}>id: {id}</p>
+        <div className={styles.buttonContainer}>
           <button type='button' onClick={onCancel}>
             Cancel
           </button>
