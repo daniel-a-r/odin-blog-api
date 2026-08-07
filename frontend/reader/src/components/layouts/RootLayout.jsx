6035dcf8cec42c12dd5e21251280852b05534508
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2026-08-07 02:36:18 -0400

    feat: use auth status context for dynamic buttons

--- frontend/reader/src/components/layouts/RootLayout.jsx
+++ frontend/reader/src/components/layouts/RootLayout.jsx
@@ -1,8 +1,14 @@
 import { Button } from '@/components/ui/button';
-import { Outlet } from 'react-router';
-import { Link } from 'react-router';
+import { Outlet, Link } from 'react-router';
+import { useAuthStatus } from '@/app/AuthStatusProvider';
 
 const RootLayout = () => {
+  const { authStatus } = useAuthStatus();
+
+  const handleSignOut = () => {
+    console.log('Sign out clicked');
+  };
+
   return (
     <div className='mx-auto grid h-svh max-w-5xl grid-rows-[max-content_1fr] px-8'>
       <div className='flex justify-between py-4'>
@@ -10,12 +16,20 @@ const RootLayout = () => {
           <Button variant='ghost'>Danny&apos;s Blog</Button>
         </Link>
         <div className='flex gap-2'>
-          <Link to='login'>
-            <Button variant='outline'>Login</Button>
-          </Link>
-          <Link to='sign-up'>
-            <Button variant='outline'>Sign Up</Button>
-          </Link>
+          {authStatus ? (
+            <Button variant='outline' onClick={handleSignOut}>
+              Sign Out
+            </Button>
+          ) : (
+            <>
+              <Link to='login'>
+                <Button variant='outline'>Login</Button>
+              </Link>
+              <Link to='sign-up'>
+                <Button variant='outline'>Sign Up</Button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
       <div className='p-4'>
