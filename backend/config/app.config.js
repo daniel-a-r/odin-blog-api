3ca1b66777ffeba67f230f45a8e5e3cd81d1de2b
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2026-08-09 01:41:10 -0400

    fix: cors for localhost

--- backend/config/app.config.js
+++ backend/config/app.config.js
@@ -2,9 +2,11 @@ import express from 'express';
 import helemt from 'helmet';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
+// app.use(cors({ origin: ['http://localhost:5174'] }));
 app.use(helemt());
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
