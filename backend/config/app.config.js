0c389d2d246e3402db6262d76a701b7132a43694
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2026-08-09 07:35:14 -0400

    feat: save logs to file in prod

--- backend/config/app.config.js
+++ backend/config/app.config.js
@@ -3,13 +3,23 @@ import helemt from 'helmet';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import cors from 'cors';
+import { createWriteStream } from 'node:fs';
+import path from 'node:path';
 
 const app = express();
+const accessLogStream = createWriteStream(
+  path.join(import.meta.dirname, 'access.log'),
+  { flags: 'a' },
+);
 
 app.use(cors({ origin: ['http://localhost:5174'] }));
 app.use(helemt());
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(process.env.NODE_ENV === 'DEV' ? morgan('dev') : morgan('combined'));
+app.use(
+  process.env.NODE_ENV === 'DEV'
+    ? morgan('dev')
+    : morgan('combined', { stream: accessLogStream }),
+);
 
 export default app;
