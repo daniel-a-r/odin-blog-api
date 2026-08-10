a1372e47a9917a492a91a5c856ccb4d4140176ce
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2026-08-10 01:19:37 -0400

    refactor: make variable name more descriptive

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -93,7 +93,7 @@ const validateSignUp = [
 const checkSignUpValidationErrors = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ validationErrors: errors.array() });
   }
   next();
 };
