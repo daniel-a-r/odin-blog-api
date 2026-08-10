6867b5bbb721fc0cc9c142a6472b1f8cf42265d0
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2026-08-10 01:36:12 -0400

    feat: implement user create request

--- frontend/reader/src/routes/SignUp.jsx
+++ frontend/reader/src/routes/SignUp.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Card,
   CardAction,
@@ -9,10 +10,38 @@ import {
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import api from '@/utils/api';
+import { SIGN_UP_ENDPOINT } from '@/utils/endpoints';
+import { useAuth } from '@/contexts/AuthContext';
 
 const SignUp = () => {
-  const signUp = (formData) => {
-    console.log(formData);
+  const [errorMessages, setErrorMessages] = useState([]);
+  const { accessToken, setAccessToken } = useAuth();
+
+  const handleSignUp = async (formData) => {
+    const body = {
+      username: formData.get('username'),
+      password: formData.get('password'),
+      confirmPassword: formData.get('confirmPassword'),
+    };
+
+    try {
+      const { data } = await api.post(SIGN_UP_ENDPOINT, body, {
+        withCredentials: true,
+      });
+      console.log(data);
+    } catch (error) {
+      if ((error.status = 400 && error.response?.data.validationErrors)) {
+        const { validationErrors } = error.response.data;
+        const validationErrorMessages = validationErrors.map(
+          (validationError) => validationError.msg,
+        );
+        setErrorMessages(validationErrorMessages);
+        console.log();
+      } else {
+        throw error;
+      }
+    }
   };
 
   return (
@@ -23,7 +52,7 @@ const SignUp = () => {
           <CardAction>Login</CardAction>
         </CardHeader>
         <CardContent>
-          <form action={signUp} id='signUpForm' className='grid gap-4'>
+          <form action={handleSignUp} id='signUpForm' className='grid gap-4'>
             <div className='grid gap-2'>
               <Label htmlFor='username'>Username</Label>
               <Input type='text' id='username' name='username' required />
