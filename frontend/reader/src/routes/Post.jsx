8d08b8b073cf2f10399175000e2f1387adf7284b
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2026-08-18 03:11:20 -0400

    feat: add styling to post

--- frontend/reader/src/routes/Post.jsx
+++ frontend/reader/src/routes/Post.jsx
@@ -1,16 +1,26 @@
 import { useLoaderData } from 'react-router';
+import {
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from '@/components/ui/card';
 
 const Post = () => {
   const { post } = useLoaderData();
 
-  console.log(post);
-
   return (
-    <>
-      <h1>{post.title}</h1>
-      <p>{post.createdAt}</p>
-      <p>{post.body}</p>
-    </>
+    <div className='flex flex-col gap-2'>
+      <CardHeader>
+        <CardTitle className='text-2xl'>{post.title}</CardTitle>
+        <CardDescription>
+          {new Date(post.createdAt).toLocaleDateString()}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p className='whitespace-pre-wrap'>{post.body}</p>
+      </CardContent>
+    </div>
   );
 };
 
