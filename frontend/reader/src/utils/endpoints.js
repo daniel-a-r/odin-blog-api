f474929b60e5a37597f6bd339b889d4922e86234
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2026-08-20 22:18:29 -0400

    feat: add user endpoint

--- frontend/reader/src/utils/endpoints.js
+++ frontend/reader/src/utils/endpoints.js
@@ -7,3 +7,4 @@ export const SIGN_UP_ENDPOINT = '/auth/sign-up/';
 export const LOGOUT_ENDPOINT = '/auth/logout/';
 export const REFRESH_ENDPOINT = '/auth/refresh/';
 export const READER_POST_ENDPOINT = '/reader/post/';
+export const USER_ENDPOINT = '/auth/user';
